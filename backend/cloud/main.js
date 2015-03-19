db8fe5a74ce6b6ede20e2ae5d32a690d83947ee0
Daniel Edwards <cdaniel.edwards@gmail.com>
2015-03-19 09:28:29 -0400

    Sending Emails

--- backend/cloud/main.js
+++ backend/cloud/main.js
@@ -1,6 +1,55 @@
+var Mandrill = require('mandrill');
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+function sendTemplate(templateName, params) {
+  var promise = new Parse.Promise();
+
+  Mandrill.initialize('2TD2OD-Z5Bx6fCUFI46bTg');
+  Mandrill.sendTemplate({
+    template_name: templateName,
+    template_content: [
+      {
+        name: "SUBJECT",
+        content: params.subject
+      },
+      {
+        name: "FName",
+        content: params.firstName
+      },
+      {
+        name: "LName",
+        content: params.lastName
+      }
+    ],
+    message: {
+      from_email: "rendezvousemailapp@gmail.com",
+      to: [{
+        email: params.toEmail,
+        name: params.firstName + " " + params.lastName
+      }]
+    }
+  }, {
+    success: function (httpResponse) {
+      console.log(httpResponse);
+      promise.resolve("Email sent!");
+    },
+    error: function (httpResponse) {
+      console.error(httpResponse);
+      promise.reject("Uh oh, something went wrong");
+    }
+  });
+
+  return promise;
+}
+
+Parse.Cloud.define('appointment', function(request, response) {
+  sendTemplate('mandrill-app-test-2', {
+    subject: 'YO',
+    firstName: "Dan",
+    lastName: "Edwards",
+    toEmail: "cdaniel.edwards@gmail.com"
+  }).then(function(){
+    response.success();
+  }, function(error){
+    response.error(error);
+  });
 });
